541541adc3f75262ed2143039b17acb6c416dceb
jasonjkaminsky <jason.j.kaminsky@gmail.com>
2022-06-23 08:58:45 -0500

    Add files via upload

---
+++ class12/StringDemo1.java
@@ -0,0 +1,19 @@
+package com.syntax.class12;
+
+public class StringDemo1 {
+
+	public static void main(String[] args) {
+		
+		String name = new String ("Oleg");
+		
+		String name2 = "Oleg";
+
+		System.out.println(name.length());
+		
+		name2 = "  Zameer ";
+		
+		System.out.println(name2.length());
+		
+	}
+
+}
---
+++ class12/StringDemo2.java
@@ -0,0 +1,20 @@
+package com.syntax.class12;
+
+public class StringDemo2 {
+
+	public static void main(String[] args) {
+		
+		String firstName = "Adem";
+		String lastName = "Jones";
+		
+		String name = " ";
+		
+		
+		System.out.println(name.isEmpty());
+	
+		
+		
+
+	}
+
+}
