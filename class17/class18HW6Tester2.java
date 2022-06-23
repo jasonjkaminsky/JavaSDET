66a8f455908e4ad1763c76f3ec66497920a41e0e
jasonjkaminsky <jason.j.kaminsky@gmail.com>
2022-06-22 22:33:48 -0500

    Add files via upload

---
+++ class17/class18HW6Tester2.java
@@ -0,0 +1,16 @@
+package com.syntax.class17;
+
+import com.syntax.class18.class18HW6;
+
+public class class18HW6Tester2 {
+    public static void main(String[] args) {
+
+    class18HW6 obj1 = new class18HW6("Hello"); // only this constructor is accessible outside of its class
+                                                //  and package bc it's public. The others aren't accessible.
+//    class18HW6 obj2 = new class18HW6(13);
+//    class18HW6 obj3 = new class18HW6(true);
+//    class18HW6 obj4 = new class18HW6();
+
+}
+}
+
