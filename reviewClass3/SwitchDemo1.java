28d7a1ac8186087f985cf02436bd7c33b57e8120
jasonjkaminsky <jason.j.kaminsky@gmail.com>
2022-06-24 13:27:18 -0500

    Add files via upload

---
+++ reviewClass3/SwitchDemo1.java
@@ -0,0 +1,36 @@
+package com.syntax.reviewClass3;
+
+
+    public class SwitchDemo1 {
+
+        public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            String country="Pakistan";
+
+            switch(country) {
+
+                case "Turkey":
+                    System.out.println("Ankara");
+                    break;
+                case "USA":
+                    System.out.println("DC");
+                    break;
+                case "Serbia":
+                    System.out.println("Begrade");
+                    break;
+                case "Albania":
+                    System.out.println("Tirana");
+                    break;
+                case "Afghanistan":
+                    System.out.println("Kabul");
+                    break;
+                case "Pakistan":
+                    System.out.println("Islamabad");
+                    break;
+                default:
+                    System.out.println("Not a valid country");
+            }
+
+        }
+
+    }
