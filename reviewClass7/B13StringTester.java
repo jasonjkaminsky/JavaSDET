a9c8789c8d20fc5bff5e65a986203ffbf6a8616b
jasonjkaminsky <jason.j.kaminsky@gmail.com>
2022-06-27 10:42:13 -0500

    Add files via upload

---
+++ reviewClass7/B13StringTester.java
@@ -0,0 +1,16 @@
+package com.syntax.reviewClass7;
+
+public class B13StringTester {
+
+    public static void main(String[] args) {
+
+        B13String str=new B13String("abcsssesda");
+        int count=str.countChars('s');
+        System.out.println(count);
+        str.printChars('a');
+
+        B13String str2=new B13String("jajajjaja");
+
+    }
+
+}
