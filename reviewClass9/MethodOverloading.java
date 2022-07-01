888b6ddbd8ebcf83426df5d1aa26ab4545559f1e
jasonjkaminsky <jason.j.kaminsky@gmail.com>
2022-06-30 21:14:05 -0500

    Add files via upload

---
+++ reviewClass9/MethodOverloading.java
@@ -0,0 +1,26 @@
+package com.syntax.reviewClass9;
+
+public class MethodOverloading {
+    private void method(){
+
+    }
+
+  /*  public void method(){
+    }*/
+
+    private void method(int num){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(String args) {
+
+    }
+
+
+
+
+}
