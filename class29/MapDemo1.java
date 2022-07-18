adad5fd28b02f07ef81ed7f0c3b4bc2346e28054
jasonjkaminsky <jason.j.kaminsky@gmail.com>
2022-07-17 23:11:46 -0500

    adding class29 files

---
+++ class29/MapDemo1.java
@@ -0,0 +1,26 @@
+package com.syntax.class29;
+
+import java.util.HashMap;
+
+public class MapDemo1 {
+    public static void main(String[] args) {
+
+        HashMap<String, Double> fruit= new HashMap<>();
+        fruit.put("Apple", 2000.0);// auto upcasting does not work with wrappers (2000 will not be automaticaly converted to 2000.0)
+        fruit.put("Orange", 20.0);
+        fruit.put("Kiwi", 10.2);
+        fruit.put("Bannana", 50.5);
+        fruit.put("Apple", 10.5);
+        System.out.println(fruit);
+        System.out.println(fruit.get("Kiwi")); // we get the value of kiwi
+        System.out.println(fruit.remove("Orange"));
+        System.out.println(fruit);
+        System.out.println(fruit.containsKey("Kiwi"));
+        System.out.println(fruit.containsValue(10.2));
+        System.out.println(fruit.isEmpty());
+        System.out.println(fruit.replace("Kiwi", 15.2));
+        System.out.println(fruit);
+        fruit.put(null,null);
+        System.out.println(fruit);// we can insert null key and value on hashMap but not on a treemap
+    }
+}
